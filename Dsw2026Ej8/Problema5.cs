11261ec98beac2ec9535777788c46c1a28c4e9c2
Amado Maximiliano <Maximiliano.Amado@alu.frt.utn.edu.ar>
2026-05-04 19:38:58 -0300

    Resolucion problema 5

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+        
+        public class Sale // clase base del problema 
+        {
+            public decimal Amount { get; set; }
+
+            public Sale(decimal amount)
+            {
+                Amount = amount;
+            }
+
+            public virtual decimal CalculateTotal()
+            {
+                return Amount;
+            }
+        }
+
+   
+        public class RetailSale : Sale
+        {
+            public RetailSale(decimal amount) : base(amount) { }
+
+            public override decimal CalculateTotal()
+            {
+                return Amount;
+            }
+        }
+
+        public class WholesaleSale : Sale
+        {
+            private const decimal Descuento = 0.10m;
+
+            public WholesaleSale(decimal amount) : base(amount) { }
+
+            public override decimal CalculateTotal()
+            { 
+                return Amount * (1 - Descuento);
+            }
+        }
+ 
+    
+
+}

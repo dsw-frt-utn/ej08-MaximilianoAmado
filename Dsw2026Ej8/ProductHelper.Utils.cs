876d9abff5bed99168a53fb2e79543ae6d2361f7
Amado Maximiliano <Maximiliano.Amado@alu.frt.utn.edu.ar>
2026-05-04 19:14:59 -0300

    Resolucion problema 1

---
+++ Dsw2026Ej8/ProductHelper.Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        private const string CultureMoneda = "es-AR"; 
+
+        private string FormatearPrecio(decimal price)
+        {
+            var cultura = new CultureInfo(CultureMoneda);
+            return price.ToString("C", cultura);
+        }
+    }
+}

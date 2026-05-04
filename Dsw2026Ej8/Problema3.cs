355338aa37ad77314013834d151a479d8c360877
Amado Maximiliano <Maximiliano.Amado@alu.frt.utn.edu.ar>
2026-05-04 19:28:16 -0300

    Resolucion problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public  class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        { 
+           int copiaValor = originalValue;
+           copiaValor++;
+           Product copiaProducto = product;
+           copiaProducto.UpdateDescription("Descripción modificada");
+           return $"{originalValue}-{copiaValor}-{product.Description}";
+        }
+
+        public class Product
+        {
+            public string Description { get; private set; }
+            public Product(string description)
+            {
+                Description = description;
+            }
+            public void UpdateDescription(string newDescription)
+            {
+                Description = newDescription;
+            }
+        }
+    }
+}

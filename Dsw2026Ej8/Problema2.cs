955c14f87e8b7b3014f5742542d75b9c13de364c
Amado Maximiliano <Maximiliano.Amado@alu.frt.utn.edu.ar>
2026-05-04 19:21:37 -0300

    Resolucion problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(
+        long productCode,
+        string productDescription,
+        int quantity,
+        decimal unitPrice)
+        {
+           
+            decimal total = quantity > 0
+                ? quantity * unitPrice
+                : 0m;
+
+            var resumen = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total
+            };
+
+            return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,10 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            var helper = new ProductHelper();
-            string etiqueta = helper.ObtenerEtiquetaProducto(1001, "Notebook Lenovo", 450000.99m);
-            Console.WriteLine(etiqueta);
+         
         }
     }
 }

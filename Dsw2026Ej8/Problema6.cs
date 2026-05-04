65dd42f8f8e1162118d540e9c4be4a4ba19cd0ae
Amado Maximiliano <Maximiliano.Amado@alu.frt.utn.edu.ar>
2026-05-04 19:45:51 -0300

    Resolucion problema 6

---
+++ Dsw2026Ej8/Problema6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            if (code == null)
+            {
+                return "SIN-CODIGO";
+            }
+            return code.ToProductCode();
+        }
+    }
+
+    public static class Extensions
+    {
+ 
+        public static string ToProductCode(this string code)
+        { 
+            string resultado = code.Trim();
+
+            resultado = resultado.ToUpper();
+
+            while (resultado.Contains("  "))
+            {
+                resultado = resultado.Replace("  ", " ");
+            }
+
+            resultado = resultado.Replace(' ', '-');
+
+            return resultado;
+        }
+    }
+}
